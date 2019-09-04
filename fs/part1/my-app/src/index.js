08ee2badfd45814b0cea8d25eccc2887cd5004cd
MuyMalo <alexjmalo@gmail.com>
2019-09-04 16:03:25 -0500

    first upload

---
+++ fs/part1/my-app/src/index.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const Hello = (props) => {
+    return (
+        <div>
+            <p>Hello {props.name}</p>
+        </div>
+    )
+}
+
+const App = () => {
+    return (
+        <div>
+            <h1>Greetings</h1>
+            <Hello name = "George"/>
+            <Hello name = "George"/>
+            <Hello name = "George"/>
+        </div>
+    )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
