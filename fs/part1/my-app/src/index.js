1214b05023f889f372eb68a1cbae539854a866dd
MuyMalo <alexjmalo@gmail.com>
2019-09-10 14:44:17 -0500

    state hook

--- fs/part1/my-app/src/index.js
+++ fs/part1/my-app/src/index.js
@@ -1,22 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
 const App = (props) => {
-    const {counter} = props 
+    const [counter, setCounter] = useState(0) 
+
+    setTimeout(
+        () => setCounter(counter + 1),
+        1000
+    )
     return (
         <div>{counter}</div>
     )
 }
 
-let counter = 1
-
-const refresh = () => {
-    ReactDOM.render(
-        <App counter ={counter} />, 
-        document.getElementById('root')
-    )
-}
-
-refresh()
-counter += 1
-refresh()
+ReactDOM.render(
+    <App />, 
+    document.getElementById('root')
+)
